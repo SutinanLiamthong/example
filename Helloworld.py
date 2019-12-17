79246f6e111a02238ead80649d4d705da3c90bd4
Nantipat <nantipat.k@hotmail.com>
2019-12-17 11:05:09 +0700

    efite test

--- Helloworld.py
+++ Helloworld.py
@@ -1,12 +1,5 @@
 print ("My name is Nut")
 print ("My name is Pop")
 print ("My name is May")
-<<<<<<< HEAD
 print ("My name is Ning")
 print ("My name is Jr")
-=======
-print ("My name is Oat")
-print ("My name is Jr")
-
-
->>>>>>> 000e886d2ccc1f389617e2c115a6d435eadfc29c
